f88c992082f87bd276ae48436bafcd667e801a6f
hsgo2430 <hsgo2430@naver.com>
2024-03-12 00:23:16 +0900

    블루투스 기기연결 기능 추가(오류 수정 필요)

--- Engineering_App/app/src/main/java/com/example/engineering_app/activity/BluetoothActivity.kt
+++ Engineering_App/app/src/main/java/com/example/engineering_app/activity/BluetoothActivity.kt
@@ -4,10 +4,15 @@ import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.os.SystemClock
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -16,6 +21,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.engineering_app.databinding.ActivityBluetoothBinding
 import com.example.engineering_app.utils.Extension.showMessage
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.nio.charset.Charset
 import java.util.UUID
 
 
@@ -34,14 +44,20 @@ class BluetoothActivity : AppCompatActivity() {
         bluetoothManager.adapter
     }
 
+    lateinit var mBluetoothDevice: BluetoothDevice
+    lateinit var mBluetoothSocket: BluetoothSocket
+    private lateinit var mBluetoothHandler: Handler
+    var mThreadConnectedBluetooth: ConnectedBluetoothThread? = null
+
     private val adapter: ArrayList<Pair<String, String>> = ArrayList()
+    private val pairing_device: ArrayList<String> = ArrayList()
 
     private val activityResultLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == RESULT_OK) {
-                showMessage(this, "블루투스 활성화")
+                showMessage("블루투스 활성화")
             } else if (it.resultCode == RESULT_CANCELED) {
-                showMessage(this, "취소")
+                showMessage("취소")
             }
         }
 
@@ -50,6 +66,7 @@ class BluetoothActivity : AppCompatActivity() {
     companion object {
         private const val REQUEST_LOCATION_PERMISSION = 1
         private const val REQUEST_BLUETOOTH_CONNECT_PERMISSION = 1
+        private const val BT_MESSAGE_READ = 2
 
     }
 
@@ -71,6 +88,26 @@ class BluetoothActivity : AppCompatActivity() {
         binding.connectBtn.setOnClickListener {
             getPairedDevices()
         }
+
+        binding.sendDataBtn.setOnClickListener {
+            if(mThreadConnectedBluetooth != null){
+                mThreadConnectedBluetooth!!.write(binding.insertSendDataTv.text.toString())
+                binding.insertSendDataTv.text.clear()
+            }
+            else{
+                showMessage("mThreadConnectedBluetooth is null")
+            }
+        }
+
+        mBluetoothHandler = object : Handler(Looper.getMainLooper()) {
+            override fun handleMessage(msg: Message) {
+                if (msg.what == BT_MESSAGE_READ) {
+                    val readMessage = (msg.obj as? ByteArray)?.toString(Charset.forName("UTF-8"))
+                    binding.receiveDataTv.text = readMessage
+                }
+            }
+        }
+
     }
 
     private fun bluetoothOn() {
@@ -85,37 +122,54 @@ class BluetoothActivity : AppCompatActivity() {
     }
 
     private fun getPairedDevices() {
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) { // 지금 버전이 안드로이드 10보다 크면
             if (ActivityCompat.checkSelfPermission(
                     this,
                     Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_SCAN
+                ) != PackageManager.PERMISSION_GRANTED||
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
                 // 권한이 부여되지 않았다면, 사용자에게 권한 요청
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.BLUETOOTH_SCAN,
+                        Manifest.permission.BLUETOOTH_CONNECT,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.BLUETOOTH_ADMIN,
+                        Manifest.permission.BLUETOOTH
+                    ),
                     REQUEST_BLUETOOTH_CONNECT_PERMISSION
                 )
-                showMessage(this, "권한 부여 필요")
             }
             else{
                 bluetoothAdapter?.let {
                     if (it.isEnabled) {
                         val pairedDevices: Set<BluetoothDevice> = it.bondedDevices
-                        Log.d("로그", it.bondedDevices.toString())
                         if (pairedDevices.isNotEmpty()) {
                             adapter.clear()
+                            pairing_device.clear()
                             pairedDevices.forEach { device ->
                                 adapter.add(Pair(device.name, device.address))
+                                pairing_device.add(device.name)
                             }
-                            showMessage(this, adapter.toString())
+                            MaterialAlertDialogBuilder(this)
+                                .setTitle("페어링할 기기를 선택해 주세요")
+                                .setItems(pairing_device.toTypedArray()) { dialog, position -> connectSelectedDevice(pairing_device[position], pairedDevices) }
+                                .show()
+
                         } else {
                             Toast.makeText(this, "페어링된 장치가 없습니다.", Toast.LENGTH_SHORT).show()
                         }
                     }
                     else{
-                        showMessage(this, "블루투스")
+                        showMessage("블루투스")
                     }
                 }
             }
@@ -127,15 +181,21 @@ class BluetoothActivity : AppCompatActivity() {
                     Log.d("로그", it.bondedDevices.toString())
                     if (pairedDevices.isNotEmpty()) {
                         adapter.clear()
+                        pairing_device.clear()
                         pairedDevices.forEach { device ->
                             adapter.add(Pair(device.name, device.address))
+                            pairing_device.add(device.name)
                         }
-                        showMessage(this, adapter.toString())
+                        MaterialAlertDialogBuilder(this)
+                            .setTitle("페어링할 기기를 선택해 주세요")
+                            .setItems(pairing_device.toTypedArray()) { dialog, position -> connectSelectedDevice(pairing_device[position], pairedDevices) }
+                            .show()
+
                     } else {
                         Toast.makeText(this, "페어링된 장치가 없습니다.", Toast.LENGTH_SHORT).show()
                     }
                 } else {
-                    showMessage(this, "블루투스")
+                    showMessage("블루투스")
                 }
             }
         }
@@ -151,13 +211,139 @@ class BluetoothActivity : AppCompatActivity() {
             REQUEST_BLUETOOTH_CONNECT_PERMISSION -> {
                 // 요청한 권한이 모두 부여되었는지 확인
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // 권한이 부여되었다면, 여기에 블루투스 연결 로직을 추가
+                    showMessage("권한이 부여되었습니다.")
+                    Log.d("권한", grantResults.isNotEmpty().toString())
+                    Log.d("권한", grantResults[0].toString())
+                    Log.d("권한", PackageManager.PERMISSION_GRANTED.toString())
                 } else {
-                    // 권한이 거부되었다면, 사용자에게 권한이 필요한 이유를 설명하거나
-                    // 기능을 사용할 수 없음을 알리는 로직을 추가
+                    showMessage("해당 기능을 지원하지 않습니다.")
+                    Log.d("권한", grantResults.isNotEmpty().toString())
+                    Log.d("권한", grantResults[0].toString())
+                    Log.d("권한", PackageManager.PERMISSION_GRANTED.toString())
                 }
                 return
             }
         }
     }
+
+    fun connectSelectedDevice(selected_device: String, pairing_device: Set<BluetoothDevice>){
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+            mBluetoothDevice = pairing_device.firstOrNull { device_list ->
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    showMessage("권한 부여 필요")
+                    return
+                }
+                else{
+                    selected_device == device_list.name
+                }
+            }!!
+        }
+        else{
+            mBluetoothDevice = pairing_device.firstOrNull { device_list -> selected_device == device_list.name}!!
+        }
+
+        /*try {
+            mBluetoothSocket = mBluetoothDevice.createRfcommSocketToServiceRecord(BT_UUID)
+            Log.d("로그 소켓", mBluetoothSocket.toString())
+            mBluetoothSocket.connect()
+            Log.d("로그 연결", "연결")
+            mThreadConnectedBluetooth = ConnectedBluetoothThread(mBluetoothSocket, mBluetoothHandler)
+            Log.d("로그 스레드", "스레드")
+            mThreadConnectedBluetooth!!.start()
+            Log.d("로그 스레드 시작", "시작")
+        }
+        catch (e: IOException){
+            showMessage("블루투스 연결 중 오류가 발생했습니다.")
+        }*/
+        Thread {
+            try {
+                mBluetoothSocket = mBluetoothDevice.createInsecureRfcommSocketToServiceRecord(BT_UUID)
+                mBluetoothSocket.connect() // 비동기적 연결 시도
+                // 연결 성공 로그
+                Log.d("Bluetooth", "연결 성공")
+
+                // UI 스레드에서 UI 업데이트
+                runOnUiThread {
+                    // 예: 연결 상태 표시 업데이트
+                }
+
+                // 연결된 Bluetooth 스레드 시작
+                mThreadConnectedBluetooth = ConnectedBluetoothThread(mBluetoothSocket, mBluetoothHandler)
+                mThreadConnectedBluetooth!!.start()
+            } catch (e: IOException) {
+                // 연결 실패 로그
+                Log.e("Bluetooth", "연결 실패", e)
+
+                // UI 스레드에서 에러 메시지 표시
+                runOnUiThread {
+                    showMessage("블루투스 연결 중 오류가 발생했습니다.")
+                }
+            }
+        }.start()
+    }
+
+
+
+    class ConnectedBluetoothThread(socket: BluetoothSocket, mBluetoothHandler: Handler) : Thread() {
+        private val mmSocket: BluetoothSocket = socket
+        private val mmInStream: InputStream?
+        private val mmOutStream: OutputStream?
+        private val mBluetoothHandler = mBluetoothHandler
+
+        init {
+            var tmpIn: InputStream? = null
+            var tmpOut: OutputStream? = null
+
+            try {
+                tmpIn = socket.inputStream
+                tmpOut = socket.outputStream
+            } catch (e: IOException) {
+                showMessage("에러")
+            }
+
+            mmInStream = tmpIn
+            mmOutStream = tmpOut
+        }
+
+        override fun run() {
+            val buffer = ByteArray(1024)
+            var bytes: Int
+
+            while (true) {
+                try {
+                    bytes = mmInStream?.available() ?: 0
+                    if (bytes != 0) {
+                        SystemClock.sleep(100)
+                        bytes = mmInStream?.available() ?: 0
+                        bytes = mmInStream?.read(buffer, 0, bytes) ?: 0
+                        mBluetoothHandler.obtainMessage(BT_MESSAGE_READ, bytes, -1, buffer).sendToTarget()
+                    }
+                } catch (e: IOException) {
+                    break
+                }
+            }
+        }
+
+        fun write(str: String) {
+            val bytes = str.toByteArray()
+            try {
+                mmOutStream?.write(bytes)
+            } catch (e: IOException) {
+                showMessage("데이터 전송 중 오류가 발생했습니다.")
+            }
+        }
+
+        fun cancel() {
+            try {
+                mmSocket.close()
+            } catch (e: IOException) {
+                showMessage( "소켓 해제 중 오류가 발생했습니다.")
+            }
+        }
+    }
+
 }
